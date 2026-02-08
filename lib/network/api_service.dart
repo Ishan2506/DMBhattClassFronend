f3e6f3d068e1d08ba0ee8d1ce36fa59bcfc9e01f
devshah120 <devshah120902@gmail.com>
2026-02-08 21:36:12 +0530

    reffer and earn api  integration done

--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http_parser/http_parser.dart'; // Make sure http_parser is in pu
 import 'package:dm_bhatt_tutions/model/registration_payload.dart';
 
 class ApiService {
-   static const String baseUrl = "https://dmbhatt-api.onrender.com/api";
+  static const String baseUrl = "https://dmbhatt-api.onrender.com/api";
   //static const String baseUrl = "http://localhost:5000/api";
 
   static Future<http.Response> getExploreProducts() async {
