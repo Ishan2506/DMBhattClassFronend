77d60ee8fa892ff97009330817721f400e4f328f
devshah120 <devshah120902@gmail.com>
2026-03-22 12:33:23 +0530

    Make CI script executable

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+echo "Installing Flutter"
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter doctor
+
+echo "Installing dependencies"
 flutter pub get
+
+echo "Installing CocoaPods"
 cd ios
 pod install
