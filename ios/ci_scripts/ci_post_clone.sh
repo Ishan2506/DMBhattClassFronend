11f8bd225e9d346d508432a96e876a6c0e9420ce
devshah120 <devshah120902@gmail.com>
2026-06-14 16:30:28 +0530

    fix

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 set -e
 
+# Install Flutter
+rm -rf "$HOME/flutter"
+echo "Cloning Flutter repository..."
+if git clone https://github.com/flutter/flutter.git --depth 1 --branch stable --single-branch "$HOME/flutter"; then
+  echo "Flutter cloned successfully"
+else
+  echo "Clone failed, retrying..."
+  git clone https://github.com/flutter/flutter.git --depth 1 --branch stable --single-branch "$HOME/flutter"
+fi
+
+export PATH="$HOME/flutter/bin:$PATH"
+export FLUTTER_HOME="$HOME/flutter"
+which flutter || (echo "Flutter not found in PATH" && exit 1)
+
 echo "Flutter version:"
 flutter --version
 
